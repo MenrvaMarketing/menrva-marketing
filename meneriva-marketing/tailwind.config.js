e00e996fe28196b68f17e5e2316b49aae6be0fbf
MenrvaMarketing <tcjoseph30@aol.com>
2025-06-13 21:18:06 -0700

    Add files via upload

---
+++ meneriva-marketing/tailwind.config.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        gold: '#FFD700',
+        navy: '#0f172a',
+      },
+    },
+  },
+  plugins: [],
+}
